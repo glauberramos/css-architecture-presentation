f9fa4014ece2fd86ecc6958a66f792846dd237f2
Glauber Ramos <glauberamos@gmail.com>
2013-10-08 01:24:17 -0300

    removing template stuff - first commit

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Arquitetura de CSS',
+    subtitle: 'Glauber Ramos',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: '',
+    company: '',
+    twitter: '@glauberamos',
+    www: 'http://www.glauberramos.com',
+    github: 'http://github.com/glauberramos'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
